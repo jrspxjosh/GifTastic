06f3721a543bd800b9164dca95d5c63076783a8d
Joshua Serrano-Patron <jrsp@Joshuas-MacBook-Pro.local>
2017-06-19 14:55:06 -0400

    Working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var animals = ["dog", "cat", "bird", "monkey", "gorilla", "duck", "hamster", "fi
 function displayImage() {
 
 	var animalName = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=10&offset=0";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalName + "&api_key=dc6zaTOxFJmzC&limit=10&offset=0";
 
 	$("#animals").empty();
 
